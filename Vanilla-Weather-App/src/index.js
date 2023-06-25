336a7f5c672793a979d7958d691207e28b6c81ec
Taravat Yazdani <ytaravat@yahoo.com>
2023-06-25 14:16:04 +0330

    Changed weather details in Js

--- Vanilla-Weather-App/src/index.js
+++ Vanilla-Weather-App/src/index.js
@@ -1,1 +1,37 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+date.innerHTML = `${day} ${hours}:${minutes}`;
 
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+let city = "Tehran";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=aca4dd3643b89e94dbd3cac6cf6f2638`;
+axios.get(apiUrl).then(showTemperature);
