d552114e148db9426e4744bdc8160c89c9e5011e
Taravat Yazdani <ytaravat@yahoo.com>
2023-06-25 16:33:49 +0330

    Added the relevant icon from API

--- Vanilla-Weather-App/src/index.js
+++ Vanilla-Weather-App/src/index.js
@@ -25,11 +25,17 @@ function showTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
 let city = "Tehran";
